418b72217f9b5ba76adc7fca9d2c438596346ae7
Manuel Garcia F. <mgarciafrigo@gmail.com>
2024-04-19 17:40:29 -0300

    arreglar errores de mvn verify

--- src/main/java/ar/edu/utn/frba/dds/Uniforme.java
+++ src/main/java/ar/edu/utn/frba/dds/Uniforme.java
@@ -6,14 +6,26 @@ public class Uniforme {
   private Prenda calzado;
 
   public Uniforme(Prenda prendaSuperior, Prenda prendaInferior, Prenda calzado) {
-    if (!prendaSuperior.esDeCategoria(Categoria.PARTE_SUPERIOR) ||
-        !prendaInferior.esDeCategoria(Categoria.PARTE_INFERIOR) ||
-        !calzado.esDeCategoria(Categoria.CALZADO)) {
+    if (!prendaSuperior.esDeCategoria(Categoria.PARTE_SUPERIOR)
+        || !prendaInferior.esDeCategoria(Categoria.PARTE_INFERIOR)
+        || !calzado.esDeCategoria(Categoria.CALZADO)) {
       throw new RuntimeException("Las prendas no son de las categorias correctas");
     }
 
     this.prendaSuperior = prendaSuperior;
     this.prendaInferior = prendaInferior;
     this.calzado = calzado;
+  }
+
+  public Prenda getPrendaSuperior() {
+    return prendaSuperior;
+  }
+
+  public Prenda getPrendaInferior() {
+    return prendaInferior;
+  }
+
+  public Prenda getCalzado() {
+    return calzado;
   }
 }
